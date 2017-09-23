8673f5f5cec83a12999c5ed64ec95aae07630543
jcadima <juanjcadima@gmail.com>
2017-09-22 22:16:57 -0400

    Added TaskFiles Model

---
+++ app/TaskFiles.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TaskFiles extends Model
+{
+    protected $fillable = ['task_id', 'filename'];
+    
+    public function task()
+    {
+        return $this->belongsTo('App\Task');
+    }
+}
