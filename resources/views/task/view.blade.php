a4902003d1952499c4e212524e740b16a3325267
jcadima <juanjcadima@gmail.com>
2017-11-01 14:34:44 -0400

    changed to bootstrap panels

--- resources/views/task/view.blade.php
+++ resources/views/task/view.blade.php
@@ -10,11 +10,8 @@ task_view->id: {{ $task_view->id }}<br>
 -->
 
 
-<div class="col-md-7">
+<div class="col-md-8">
     <h1>{{ $task_view->task_title }}</h1>
-
-
-
 
     <div class="form-group">
         <label>Description:</label>
@@ -27,88 +24,121 @@ task_view->id: {{ $task_view->id }}<br>
         <a class="btn btn-default" href="{{ redirect()->getUrlGenerator()->previous() }}">Go Back</a>
     </div>
 
+    <div class="row">
+        <hr>
+        @if( count($images_set) > 0 ) 
+            <div class="col-md-6">
+
+                <div class="panel panel-jc">
+                    <div class="panel-heading">Images</div>
+                    <div class="panel-body">
+                        <ul id="images_col">
+                            @foreach ( $images_set as $image )
+                                <li> 
+                                    <a target="_blank"href="<?php echo asset("images/$image") ?>"><img class="img-responsive" src="<?php echo asset("images/$image") ?>"></a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        @endif
+
+
+        
+        @if( count($files_set) > 0 ) 
+            <div class="col-md-6">
+
+                <div class="panel panel-jc">
+                    <div class="panel-heading">Files</div>
+                    <div class="panel-body">
+                        <ul id="images_col">
+                            @foreach ( $files_set as $file )
+                                <li> 
+                                    <a target="_blank" href="<?php echo asset("images/$file") ; ?>">{{ $file }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        @endif
+
+
+    </div>
+
+
+
 </div>
 
-<div class="col-md-5">
+<div class="col-md-4">
 
-    <div class="form-group">
-            <strong>Project:&nbsp;</strong>
+
+    <div class="panel panel-jc">
+        <div class="panel-heading">Project</div>
+        <div class="panel-body">
             <span class="label label-jc">
                 <a href="{{ route('task.list', [ 'projectid' => $task_view->project->id ]) }}">{{ $task_view->project->project_name }}</a>
             </span>
+        </div>
     </div>
 
-    <div class="form-group">
-        @if ( $task_view->priority == 0 )
-            <strong>Priority: </strong><span class="label label-info">Normal</span>
-        @else
-            <strong>Priority: </strong><span class="label label-danger">High</span>
-        @endif
+    <div class="panel panel-jc">
+        <div class="panel-heading">Priority</div>
+        <div class="panel-body">
+            @if ( $task_view->priority == 0 )
+                <span class="label label-info">Normal</span>
+            @else
+                <span class="label label-danger">High</span>
+            @endif
+        </div>
     </div>
 
-    <div class="form-group">
-        @if ( $task_view->completed == 0 )
-            <strong>Status:&nbsp;&nbsp;&nbsp;&nbsp;</strong><span class="label label-warning">Open</span>
-        @else
-            <strong>Status:&nbsp;&nbsp;&nbsp;</strong><span class="label label-success">Closed</span>
-        @endif
+    <div class="panel panel-jc">
+        <div class="panel-heading">Status</div>
+        <div class="panel-body">
+            @if ( $task_view->completed == 0 )
+                <span class="label label-warning">Open</span>
+                @if ( $is_overdue )
+                    <span class="label label-danger">Overdue</span>
+                @else
+                    <p><br>{{ $diff_in_days }} days left to complete this task</p>
+                @endif                
+            @else
+                <span class="label label-success">Closed</span>
+            @endif
+        </div>
     </div>
 
     <hr>
 
-    <div class="form-group">
-        <strong>Task Created: </strong><br>{{ $formatted_from }} <br>
-        <strong>Due Date: </strong><br>{{ $formatted_to }}  <br>
-        <hr>
-        @if ( $task_view->completed != 1 )
-            @if ( $is_overdue )
-                <span class="label label-danger">Overdue</span>
-            @else
-                {{ $diff_in_days }} days left to complete this task<br>
-            @endif
-        @endif
+    <div class="panel panel-jc">
+        <div class="panel-heading">Created</div>
+        <div class="panel-body">
+            {{ $formatted_from }} 
+        </div>
+    </div>
+
+    <div class="panel panel-jc">
+        <div class="panel-heading">Due Date</div>
+        <div class="panel-body">
+            {{ $formatted_to }} 
+        </div>
+    </div>
+
+    <div class="panel panel-jc">
+        <div class="panel-heading">Alert</div>
+        <div class="panel-body">
+            {{ $formatted_to }} 
+        </div>
     </div>
 
 </div>
 
 
-<div class="row">
-    @if( count($images_set) > 0 ) 
-        <div class="col-md-6">
-            <h1>Images:</h1>
 
-            <div>
-                <ul id="images_col">
-                @foreach ( $images_set as $image )
-                    <li> 
-                        <a target="_blank"href="<?php echo asset("images/$image") ?>"><img class="img-responsive" src="<?php echo asset("images/$image") ?>"></a>
-                    </li>
-                @endforeach
-                </ul>
-            </div>
-        </div>
-    @endif
-
-
-    
-    @if( count($files_set) > 0 ) 
-        <div class="col-md-6">
-            <h1>Files:</h1>
-
-            <div>
-                <ul id="images_col">
-                @foreach ( $files_set as $file )
-                    <li> 
-                        <a target="_blank" href="<?php echo asset("images/$file") ; ?>">{{ $file }}</a>
-                    </li>
-                @endforeach
-                </ul>
-            </div>
-        </div>
-    @endif
-
-
-</div>
 
 @stop
 
