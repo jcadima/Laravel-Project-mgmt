c0a6d07feb83cc15e8afd7f7b56706008532067c
jcadima <juanjcadima@gmail.com>
2017-11-03 14:30:17 -0400

    create user and projects via modals

--- resources/views/project/projects.blade.php
+++ resources/views/project/projects.blade.php
@@ -5,6 +5,50 @@
 
 <h1>LIST OF ACTIVE PROJECTS</h1>
 
+<div class="new_project">
+  <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>&nbsp;Add New Project</button>
+</div>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Enter Project Title</h4>
+      </div>
+      <div class="modal-body">
+        <form id="project_form" action="{{ route('project.store') }}" method="POST">
+            {{ csrf_field() }}
+
+        <div class="row">
+            <div class="col-md-12">
+            <div class="form-group">
+              <input type="text" class="form-control" id="project" name="project">
+            </div>
+          </div>
+
+        </div>
+
+        <div class="modal-footer">
+          <input class="btn btn-primary" type="submit" value="Submit" >
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+
+        </form>
+      </div>
+
+    </div>
+
+  </div>
+</div>
+<!--  END modal  -->
+
+
+
+<div class="table-responsive">
 <table class="table table-striped">
     <thead>
       <tr>
@@ -37,7 +81,7 @@
 
 
 </table>
-
+</div>
 
 
 
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -8,6 +8,74 @@
     </div>
 </div>
 
+
+<div class="new_project">
+  <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>&nbsp;Add New User</button>
+</div>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Enter User Information</h4>
+        </div>
+
+        <div class="modal-body">
+        <form id="task_form" action="{{ route('user.store') }}" method="POST">
+            {{ csrf_field() }}
+            <div class="row">
+                <div class="col-md-7">
+                    <label>Create new User <span class="glyphicon glyphicon-plus" aria-hidden="true"></span></label>
+
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Enter User Full Name" name="name" value="{{ old('name') }}">
+                        </div>
+
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Enter User Email" name="email" value="{{ old('email') }}">
+                        </div>
+
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Enter User Password" name="password">
+                        </div>
+
+                </div>
+
+                <div class="col-md-5">
+                    <div class="form-group">
+                        <label>Set Status <span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span></label>
+                        <select name="admin" class="form-control">
+                            <option value="0" selected>Disabled (default)</option>
+                            <option value="1">Active</option>
+                        </select>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="modal-footer">
+                <input class="btn btn-primary" type="submit" value="Submit" >
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+
+
+        </form>
+       </div>
+
+    </div>
+
+  </div>
+</div>
+<!--  END modal  -->
+
+
+
+
+
 <table class="table table-striped">
     <thead>
       <tr>
@@ -21,7 +89,7 @@
 @if ( !$users->isEmpty() ) 
     <tbody>
     @foreach ( $users as $user)
-    @if ( $user->id == 0 )  @continue 
+    @if ( $user->id == 1 )  @continue 
     @endif
       <tr>
         <td><a href="{{ route('user.list', ['id'=> $user->id] ) }}">{{ $user->name }}</a></td>
